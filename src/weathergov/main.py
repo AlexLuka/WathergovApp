cea02dbbcecfc537d9f17563c3df3e49543c7bd5
Alexey Lukyanov <alexey.a.lukyanov@gmail.com>
2024-07-10 19:10:14 -0500

    Add placeholders for data loader scripts

--- src/weathergov/main.py
+++ src/weathergov/main.py
@@ -1,5 +1,8 @@
 import argparse
 
+from weathergov.scripts.loaders import (observation_station_loader,
+                                        historical_data_loader,
+                                        rt_data_loader)
 
 """
     Depending on type of a selected script we are going to run the corresponding 
@@ -13,10 +16,13 @@ import argparse
 def main(script_name: str):
     if script_name == "ObservationStationsLoader":
         print(f"ObservationStationsLoader is running")
+        observation_station_loader()
     elif script_name == "HistoricalDataLoader":
         print(f"HistoricalDataLoader is running")
+        historical_data_loader()
     elif script_name == "RTDataLoader":
         print(f"RTDataLoader is running")
+        rt_data_loader()
     else:
         print(f"Unknown script {script_name}. Exiting...")
         exit(1)
--- src/weathergov/scripts/loaders.py
+++ src/weathergov/scripts/loaders.py
@@ -1,2 +1,10 @@
 def observation_station_loader():
     pass
+
+
+def historical_data_loader():
+    pass
+
+
+def rt_data_loader():
+    pass
