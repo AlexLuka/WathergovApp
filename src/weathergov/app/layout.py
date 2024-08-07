3c7be00a31b9324fadbabae3194b562c6a829bda
Alexey Lukyanov <alexey.a.lukyanov@gmail.com>
2024-08-07 13:41:04 -0500

    Clean up

--- src/weathergov/app/layout.py
+++ src/weathergov/app/layout.py
@@ -1,95 +1,15 @@
-# import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
 from dash import html, dcc
-# from plotly.express.colors import sample_colorscale
 
 from weathergov.app.viz import get_map
 from weathergov.app.components import Components
-# from weathergov.app.constants import DataLabels
 
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-
-
-# def get_map(app, colorscale='jet') -> go.Figure:
-#     #
-#     #
-#     # Get the data
-#     df = app.rc.get_observation_stations_info()
-#     app.df = df
-#
-#     # print(df.head())
-#
-#     # Create a colormap
-#     ind_nan = df['temperature'].isna()
-#
-#     # Fill nan values with mean
-#     temperature_mean = df['temperature'].mean()
-#     df.loc[ind_nan, 'temperature'] = temperature_mean
-#
-#     colorbar_tick_step = 5
-#     x_min = (int(df['temperature'].min()) // colorbar_tick_step) * colorbar_tick_step
-#     x_max = (1 + int(df['temperature'].max()) // colorbar_tick_step) * colorbar_tick_step
-#     n_ticks = 1 + int(x_max - x_min) // colorbar_tick_step
-#
-#     x_bar = np.linspace(0, 1, n_ticks)
-#     t_bar = [f"{v:.2f}°" for v in (x_bar * (x_max - x_min) + x_min).tolist()]
-#
-#     # -8.33 49.93 : that is going to be good
-#     df['temperature'] = df['temperature'].apply(lambda x: (x - x_min) / (x_max - x_min))
-#     df['color'] = sample_colorscale(colorscale, df['temperature'])
-#
-#     # Replace all the nans with gray color
-#     df.loc[ind_nan, 'color'] = 'rgba(100, 100, 100, 0.5)'
-#
-#     #
-#     #
-#     fig = go.Figure()
-#
-#     fig.add_trace(
-#         go.Scattermapbox(
-#             lat=df['latitude'],
-#             lon=df['longitude'],
-#             mode='markers',
-#             customdata=df[DataLabels.FigureCustomData],
-#             hovertemplate="<b>Station name:</b> %{customdata[0]}<br>" +
-#                           "<b>Station ID:</b> %{customdata[1]}<br><br>" +
-#                           "Latitude: %{lat:,.6f}°<br>" +
-#                           "Longitude: %{lon:.6f}°<br>" +
-#                           "Elevation: %{customdata[2]:,.1f}<br>" +
-#                           "Time zone: %{customdata[3]}<br>" +
-#                           "URL: %{customdata[4]}" +
-#                           "<extra></extra>",
-#             marker={
-#                 "color": df['color'],
-#                 "colorbar": dict(thickness=5,
-#                                  tickvals=x_bar,
-#                                  ticktext=t_bar,
-#                                  outlinewidth=0,
-#                                  orientation='h',
-#                                  y=1,
-#                                  bgcolor='white'),
-#                 "cmin": 0,
-#                 "cmax": 1,
-#                 "showscale": True,
-#                 "colorscale": colorscale
-#             },
-#         )
-#     )
-#
-#     fig.update_layout(
-#         autosize=True,
-#         mapbox_style="open-street-map",
-#         margin={"r": 0, "t": 0, "l": 0, "b": 0},
-#         mapbox_bounds={"west": -127, "east": -65, "south": 22, "north": 54},
-#         # clickmode='event+select'
-#     )
-#
-#     return fig
 
 
 def get_navbar():
